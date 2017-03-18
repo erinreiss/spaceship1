ccf6958c172902f41b66f0284d361f249ba3820e
erinreiss <reiss.aaron@gmail.com>
2017-03-19 04:50:04 +0800

    two stories and hidden audio player

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 // Set up some variables. Do I need this? What is matchdata??
 
 var headshot = $(".headshot");
+var headshotStatusQuo = $(".headshotStatusQuo");
 var matchData;
 
 
@@ -37,11 +38,47 @@ var matchData;
  //    	audio4.play();
 	// });
 
+// StatusQuo Click Listener try 1 - audio player
+$(headshotStatusQuo).click(
+	function(){
+		console.log(this.id);
+		// $('#clickStreet').css('visibility','hidden');
+		// $('#streetsBar').css('visibility','visible');
+		var thisID = ("audio/" + this.id + ".mp3");
+			console.log(thisID);
+		// for(var i=0;i<matchData.length;i++) {
+		// 	if(matchData[i].id == thisID) {
+		// 		// console.log(matchData[i].audioFill);
+		// 		$('.nameFill').text(matchData[i].nameFill);
+		// 		$('.jobFill').text(matchData[i].jobFill);
+		// 		$('.timeFill').text(matchData[i].timeFill);
+		// 		$('.fromFill').text(matchData[i].fromFill);
+		// 		$('.bioFill').html("&ldquo;" + matchData[i].bioFill + "&rdquo;");
+		// 		$('#streetTitleCh').html("&ldquo;" + matchData[i].streetTitleCh + "&rdquo;");
+		// 		$('#streetTitleEn').text(matchData[i].streetTitleEn);
+				changeTrack(thisID);	
+		// 		$('#speakerPhoto').attr("src",matchData[i].speakerPhoto)
+		// 		changeTrack(matchData[i].audioFill);		
+		//		break;
+		// 	}
+		// }
+	}
+);
+
+//Second Audio Player NOT WORKING function to reload StatusQuo audio player on each click
+
+// function changeTrack(sourceUrl) {
+//     var audioDues = $("#playerStatusQuo");      
+//     $("#audioFillStatusQuo").attr("src", sourceUrl);
+//     audioDues[0].pause();
+//     audioDues[0].load();//suspends and restores all audio element
+//     audioDues[0].play();
+// }
 
 // Interactions Click Listener try 1 - audio player
 $(headshot).click(
 	function(){
-		// console.log(this.id);
+		console.log(this.id);
 		// $('#clickStreet').css('visibility','hidden');
 		// $('#streetsBar').css('visibility','visible');
 		var thisID = ("audio/" + this.id + ".mp3");
