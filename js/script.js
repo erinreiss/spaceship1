b7483527d2b4a87e4d73e88b05ae889142878035
erinreiss <reiss.aaron@gmail.com>
2017-03-26 10:54:22 +0800

    All attempts included that allowed me to reach a final, reasonable start on a scrollable ministory. Notalbly missing: scrolling forcing you to go the ministory div

--- js/script.js
+++ js/script.js
@@ -5,36 +5,92 @@ var headshotStatusQuo = $(".headshotStatusQuo");
 var matchData;
 
 //Log Scroll amount in intro1
-// $("#intro1").scroll(function() {
+// $("#intro1inner").scroll(function() {
 //          var screenheight = parseInt($(document).height());
-//          var scrolledpx = parseInt($("#intro1").scrollTop());     
+//          var scrolledpx = parseInt($("#intro1inner").scrollTop());     
 //          var sum = screenheight+scrolledpx;
 //          console.log($("#intro1").scrollTop());
 //          console.log("screen: " + screenheight);
 //          console.log("sum=" + sum);
-//          $("div.#intro1inner").height(sum);
+//          $("div.#introRect").height(sum);
 // })
 
 //Fading landing with scroll (THIS WORKS)
 var target = $('#landing');
 var targetHeight = 200;
 // $(document).scroll(function(e){
-//     var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    // var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
 //     if(scrollPercent >= 0){
 //         target.css('opacity', scrollPercent);
 //     }
 // });
 
+//Abandoned and unnecesary attempt to make #intro1b appear w downscroll and appear w upscroll (wihtout waypoints)
+// var lastScrollTop = 0;
+// var target = $('#intro1inner');
+
+// $('#intro1inner').scroll(function(event){
+//    var changeAA = $(this).scrollTop();
+//     console.log(changeAA)
+
+//    if (changeAA > lastScrollTop){
+//        // downscroll code
+//        $('#intro1inner').scroll(function(){
+//               // console.log('changeA:' + changeAA)
+//               var scrollPercent = (250 - changeAA) / 250;
+//                 $('#intro1b').css('opacity', scrollPercent);
+//         })
+       
+//        // upscroll code
+
+//    }  else {
+//       console.log('booboo scroll up')
+        
+//       }
+//    lastScrollTop = changeAA;
+// });
+
+//make #intro1b fade in with scroll down (wihtout waypoints)
+var target = $('#intro1inner');
+$('#intro1inner').scroll(function(){
+      var changeA = target.scrollTop()
+      console.log('changeA:' + changeA)
+    // var scrollPercent = ((targetHeight / 2) - changeA) / targetHeight;
+    var scrollPercent = changeA / 250;
+    console.log('scrollPercent:' + scrollPercent)
+    // if(scrollPercent >= 0){
+        $('#intro1b').css('opacity', scrollPercent);
+    // }
+    if (scrollPercent >= .99){
+      $('#introRect').css('pointer-events', 'none')
+    }
+    else {
+      $('#introRect').css('pointer-events', 'auto')
+    }
+});
+
+//make #intro1b fade out with scroll down (wihtout waypoints)
+// var target = $('#intro1inner');
+// $('#intro1inner').scroll(function(){
+//       var changeA = target.scrollTop()
+//       console.log('changeA:' + changeA)
+//     // var scrollPercent = ((targetHeight / 2) - changeA) / targetHeight;
+//     var scrollPercent = (250 - changeA) / 250;
+//     // if(scrollPercent >= 0){
+//         $('#intro1b').css('opacity', scrollPercent);
+//     // }
+// });
+
 // Fade landing, with 200 pixels offset
-var intro1 = $('#intro1').waypoint(function (direction) {
-    console.log('bam!');
-    $(document).scroll(function(e){
-      var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
-        if(scrollPercent >= 0){
-            target.css('opacity', scrollPercent);
-        }
-    })
-}, {offset: 200});
+// var intro1 = $('#intro1').waypoint(function (direction) {
+//     console.log('bam!');
+//     $(document).scroll(function(e){
+//       var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+//         if(scrollPercent >= 0){
+//             target.css('opacity', scrollPercent);
+//         }
+//     })
+// }, {offset: 200});
 
 // Intro 1, Bringing Into Exisitence (THIS WORKS)
 // var statusQuoPhotos = $('#statusQuoPhotos').waypoint(function (direction) {
