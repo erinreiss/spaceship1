92a65ac0a236a55bed72cce6bf45a8b6e42787b3
erinreiss <reiss.aaron@gmail.com>
2017-03-30 07:50:40 +0800

    now a much more elegantWaypoints method for creating a ministory, and ministory two slides up to cover ministory 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // Set up some variables. Do I need this? What is matchdata??
-
 var headshot = $(".headshot");
 var headshotStatusQuo = $(".headshotStatusQuo");
 var matchData;
-var intro1_Top = $('#intro1').offset().top;  
-
+var intro1_Top = $('#intro1').offset().top;
+var intro2_Top = $('#intro2').offset().top;  
 
 //Delayed fading landing with scroll
 var intro1Activate = $('#intro1').waypoint(function (direction) {
@@ -27,67 +26,70 @@ var intro1Activate = $('#intro1').waypoint(function (direction) {
 //   }
 // }, {offset: 200});
 
-//Activate #intro1
+//First attempt at sticky: #intro1
+var sticky = new Waypoint.Sticky({
+  element: $('#wrapperIntro1')[0]
+})
+
+//Bring Intro1 Photos into existence and make them clickable
 var activateI1 = $('#intro1').waypoint(function (direction) {
   console.log('ba-boomba-boom!');
-$(document).scroll(function(){
-    // console.log("position relative to window: " + (intro1_Top - window.scrollY));
-    var scrollPercent = ((intro1_Top - window.scrollY) * -1) / 300;
-//     // if(scrollPercent >= 0){
-    console.log(scrollPercent);
-    $('#intro1b').css('opacity', scrollPercent);
-//     // }
-    if (scrollPercent >= .94){
-      $('.headshotStatusQuo').css('pointer-events', 'auto')
-    }
-    else {
-      $('.headshotStatusQuo').css('pointer-events', 'none')
-    }
-});
-  if(direction == 'down'){
-    $('#intro1a, #intro1b').removeClass('preActivateIntro1').addClass('activateIntro1')
-  }
-  else {
-    $('#intro1a, #intro1b').removeClass('activateIntro1').addClass('preActivateIntro1')
-  }
+  $(document).scroll(function(){
+      // console.log("position relative to window: " + (intro1_Top - window.scrollY));
+      var scrollPercent = ((intro1_Top - window.scrollY) * -1) / 300;
+      // console.log(scrollPercent);
+      $('#intro1b').css('opacity', scrollPercent);
+      if (scrollPercent >= .94){
+        $('.headshotStatusQuo').css('pointer-events', 'auto')
+      }
+      else {
+        $('.headshotStatusQuo').css('pointer-events', 'none')
+      }
+  });
 });
 
-//De-activate #intro1
-$(document).scroll(function(){
-    var scrollPercent = ((intro1_Top - window.scrollY) * -1) / 300;
-    if(scrollPercent >= 1){
-      $('#intro1a, #intro1b').removeClass('activateIntro1').addClass('deIntro1');
-    }
-});
-
-//make #intro1b fade in with scroll down (without waypoints) THIS WORKS
-// var target = $('#intro1inner');
-// $('#intro1inner').scroll(function(){
-//       var changeA = target.scrollTop()
-//       console.log('changeA:' + changeA)
-//     var scrollPercent = changeA / 250;
-//     console.log('scrollPercent:' + scrollPercent)
-//     // if(scrollPercent >= 0){
-//         $('#intro1b').css('opacity', scrollPercent);
-//     // }
-//     if (scrollPercent >= .99){
-//       $('#introRect').css('pointer-events', 'none')
-//     }
-//     else {
-//       $('#introRect').css('pointer-events', 'auto')
-//     }
-// });
-
-
-// Intro 1, Bringing Into Exisitence (THIS WORKS)
-// var statusQuoPhotos = $('#statusQuoPhotos').waypoint(function (direction) {
-//   console.log('bam!');
+// //De-activate #intro1 try 1
+// var deActiveIntro1 = $('#intro1').waypoint(function (direction) {
 //   if(direction == 'down'){
-//     $('#statusQuoPhotos').fadeTo(600,1);
-//   } else {
-//         $('#statusQuoPhotos').fadeTo(600,0);
+//     sticky.destroy()
+//     $('#wrapperIntro1').addClass("postActivateIntro1");
+//   } 
+//   else {
+//     $('#wrapperIntro1').removeClass("postActivateIntro1");
 //   }
 // }, {offset: 'bottom-in-view'});
+
+// //De-activate #intro1 try 2
+// var intro2inview = new Waypoint.Inview({
+//   element: $('#intro2')[0],
+//   enter: function(direction) {
+//     $(document).scroll(function(){
+//       console.log("introTop2: " + intro2_Top);
+//       console.log("window.scrollY: " + window.scrollY);
+//       console.log("Position2 relative to window: " + (intro2_Top - window.scrollY));
+//       var winheight = $(window).height()
+//       console.log("Window height: " + winheight);
+//       var scrollPercent2 = (intro2_Top - window.scrollY)/ (4 * winheight);
+//       console.log(scrollPercent2);
+//       $('#intro1').css('opacity', scrollPercent2);
+//       // if (scrollPercent >= .94){
+//       //   $('.headshotStatusQuo').css('pointer-events', 'auto')
+//       // }
+//       // else {
+//       //   $('.headshotStatusQuo').css('pointer-events', 'none')
+//       // }
+//   });
+//   },
+//   // entered: function(direction) {
+//   //   notify('Entered triggered with direction ' + direction)
+//   // },
+//   // exit: function(direction) {
+//   //   notify('Exit triggered with direction ' + direction)
+//   // },
+//   // exited: function(direction) {
+//   //   notify('Exited triggered with direction ' + direction)
+//   // }
+// })
 
 // photoBank Click Listener try 3 - from stackoverflow, zer00ne
 $('.photoBank').on('click', 'img', function(e) {
